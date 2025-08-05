c6194292e14d202bd31654af680a10cefdbf9e1b
Leandro Cabral <lourenco.contato1@gmail.com>
2025-08-04 22:29:57 -0300

    Update next.config.ts

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
      output: "export",
-     basePath: '/my-portfolio',
-     assetPrefix: '/my-portfolio',
 };
 
 export default nextConfig;
